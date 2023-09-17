03e555c69690529db2408d2563bffa913474bf77
Kubix007 <kuba.dolinski@hotmail.com>
2023-09-17 17:37:52 +0200

    Little fixes on frontend

--- frontend/src/providers/router/RouterProvider.tsx
+++ frontend/src/providers/router/RouterProvider.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "../../pages/HomePage";
 import LoginPage from "../../pages/LoginPage";
-import RegisterPage from "../../pages/RegisterPage.tsx";
+import RegisterPage from "../../pages/RegisterPage";
 import DashboardPage from "../../pages/DashboardPage";
 
 type Props = {};
