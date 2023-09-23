740375ca7e05e863ecfa60fc7de85effa81fdf07
michal <michal.czechowicz7@gmail.com>
2023-09-23 21:21:59 +0200

    Fixed data typing in users serializers

--- backend/friends/urls.py
+++ backend/friends/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 
 urlpatterns = [
-    path('is_friend/<int:pk>/', login_required(views.FriendsStatusView.as_view())),
+    path('send_request/<int:pk>/', login_required(views.SendRequestView.as_view())),
+    path('undo_request/<int:pk>/', login_required(views.UndoRequestView.as_view())),
+    path('accept_request/<int:pk>/', login_required(views.AcceptRequestView.as_view())),
+    path('decline_request/<int:pk>/', login_required(views.DeclineRequestView.as_view())),
+    path('remove_friend/<int:pk>/', login_required(views.RemoveFriendView.as_view())),
 ]
