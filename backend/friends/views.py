f9b0b2a001c423f08e7b5268f629a7b199855daf
michal <michal.czechowicz7@gmail.com>
2023-09-24 10:05:14 +0200

    Finished friend requests endpoints

--- backend/friends/views.py
+++ backend/friends/views.py
@@ -1,25 +1,77 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework.views import APIView
 from django.http import JsonResponse
 from .models import FriendRequest
 from django.contrib.auth.models import User
+from .serializers import FriendsRequestSerializer, CoupledUsersSerializer
+from rest_framework import status
 
 
-class SendRequestView(APIView):
-    pass
+class UsersView(APIView):
+    def get_logged_user(self):
+        return self.request.user
+
+    def get_other_user(self):
+        pk = self.kwargs.get('pk')
+        return User.objects.get(pk=pk)
 
 
-class AcceptRequestView(APIView):
-    pass
+class SendRequestView(UsersView):
+    def post(self, request, *args, **kwargs):
+        logged_user = self.get_logged_user()
+        other_user = self.get_other_user()
+
+        FriendRequest.objects.create(from_user=logged_user,
+                                     to_user=other_user)
+
+        return JsonResponse({"message": "Request sent"}, status=status.HTTP_200_OK)
 
 
-class DeclineRequestView(APIView):
-    pass
+class AcceptRequestView(UsersView):
+    def post(self, request, *args, **kwargs):
+        logged_user = self.get_logged_user()
+        other_user = self.get_other_user()
+
+        friend_request = FriendRequest.objects.get(from_user=other_user,
+                                                   to_user=logged_user)
+
+        logged_user.profile.friends.add(other_user.profile)
+        other_user.profile.friends.add(logged_user.profile)
+        friend_request.delete()
+
+        return JsonResponse({"message": "Request accepted"}, status=status.HTTP_200_OK)
 
 
-class UndoRequestView(APIView):
-    pass
+class DeclineRequestView(UsersView):
+    def post(self, request, *args, **kwargs):
+        logged_user = self.get_logged_user()
+        other_user = self.get_other_user()
+
+        friend_request = FriendRequest.objects.get(from_user=other_user,
+                                                   to_user=logged_user)
+        friend_request.delete()
+
+        return JsonResponse({"message": "Request decline"}, status=status.HTTP_200_OK)
 
 
-class RemoveFriendView(APIView):
-    pass
+class UndoRequestView(UsersView):
+    def post(self, request, *args, **kwargs):
+        logged_user = self.get_logged_user()
+        other_user = self.get_other_user()
+
+        friend_request = FriendRequest.objects.get(from_user=logged_user,
+                                                   to_user=other_user)
+        friend_request.delete()
+
+        return JsonResponse({"message": "Request undone"}, status=status.HTTP_200_OK)
+
+
+class RemoveFriendView(UsersView):
+    def post(self, request, *args, **kwargs):
+        logged_user = self.get_logged_user()
+        other_user = self.get_other_user()
+
+        other_user.profile.friends.remove(logged_user)
+        logged_user.profile.friends.remove(other_user)
+
+        return JsonResponse({"message": "Friend removed"}, status=status.HTTP_200_OK)
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from django.contrib.auth.decorators import login_required
 from . import views
 
 
@@ -6,6 +7,6 @@ urlpatterns = [
     path('register/', views.RegisterView.as_view()),
     path('login/', views.LoginView.as_view()),
     path('logout/', views.LogoutView.as_view()),
-    path('list_profiles/<str:username>/', views.UsersDataListView.as_view()),
-    path('user_data/<int:pk>/', views.UserDataRetrieveAPIView.as_view()),
+    path('list_profiles/<str:username>/', login_required(views.UsersDataListView.as_view())),
+    path('user_data/<int:pk>/', login_required(views.UserDataRetrieveAPIView.as_view())),
 ]
