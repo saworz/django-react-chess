b0df4c6755f6728f18a7f131d92d62dfba486330
michal <michal.czechowicz7@gmail.com>
2023-10-03 14:18:27 +0200

    Get sent requests list

--- backend/friends/urls.py
+++ backend/friends/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('decline_request/<int:pk>/', login_required(views.DeclineRequestView.as_view())),
     path('remove_friend/<int:pk>/', login_required(views.RemoveFriendView.as_view())),
     path('get_friends_list/', login_required(views.GetFriendListView.as_view())),
-    path('get_pending_requests/', login_required(views.GetPendingRequestsListView.as_view()))
+    path('get_pending_requests/', login_required(views.GetPendingRequestsListView.as_view())),
+    path('get_sent_requests/', login_required(views.GetSentRequestsListView.as_view())),
 ]
