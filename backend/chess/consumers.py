997151319403d825a219f5a49d66d7f9a4ba460f
michal <michal.czechowicz7@gmail.com>
2023-10-28 21:37:48 +0200

    Retrieving enemy_id from room_id

--- backend/chess/consumers.py
+++ backend/chess/consumers.py
@@ -9,6 +9,54 @@ import json
 
 
 class ChessConsumer(WebsocketConsumer):
+
+    def save_game_to_db(self, game_data):
+        sides = {
+            "white": self.game.white_pieces,
+            "black": self.game.black_pieces
+        }
+
+        white_board = {
+            "game_id": None,
+        }
+
+        black_board = {
+            "game_id": None,
+        }
+
+        for color, board in sides.items():
+            for name, piece in board.items():
+                piece_info = {
+                    "name": name,
+                    "position": piece.position,
+                    "weight": piece.weight,
+                    "possible_moves": piece.possible_moves,
+                    "capturing_moves": piece.capturing_moves,
+                    "color": piece.color,
+                }
+
+                if color == 'white':
+                    white_board[name] = piece_info
+
+                elif color == 'black':
+                    black_board[name] = piece_info
+
+        serializer = ChessGameSerializer(data=game_data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        white_board["game_id"] = serializer.data["id"]
+        black_board["game_id"] = serializer.data["id"]
+
+        white_serializer = WhiteBoardSerializer(data=white_board)
+        black_serializer = BlackBoardSerializer(data=black_board)
+
+        white_serializer.is_valid(raise_exception=True)
+        black_serializer.is_valid(raise_exception=True)
+
+        white_serializer.save()
+        black_serializer.save()
+
     def create_new_game(self, enemy_id):
         logged_user = self.scope['user']
         other_user = User.objects.get(pk=enemy_id)
@@ -24,36 +72,38 @@ class ChessConsumer(WebsocketConsumer):
             "room_id": room_id
         }
 
-        white_board = {
-            "game_id": None,
-        }
-
-        black_board = {
-            "game_id": None,
-        }
-
         print("Created new game instance")
-        self.new_game = GameInitializer()
-        self.new_game.validate_moves()
+        self.game = GameInitializer()
+        self.game.validate_moves()
+        self.save_game_to_db(game_data)
+
+    def get_enemy_id(self):
+        user = self.scope['user'].pk
+        room_id = self.scope['url_route']['kwargs']['room_id']
+
+        if str(user) == str(room_id)[:len(str(user))]:
+            enemy_id = int(str(room_id)[len(str(user)):])
+        else:
+            enemy_id = int(str(room_id)[:len(str(user)) - 1])
+        return enemy_id
 
     def connect(self):
         self.room_id = self.scope['url_route']['kwargs']['room_id']
         self.room_name = self.room_id
         self.room_group_name = f"game_{self.room_id}"
-        self.game = ChessGame.objects.filter(room_id=self.room_id).first()
 
-        if self.game:
-            async_to_sync(self.channel_layer.group_add)(
-                self.room_group_name,
-                self.channel_name
-            )
-
-            self.accept()
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
+        self.create_new_game(self.get_enemy_id())
+        self.accept()
 
     def receive(self, text_data):
         data_json = json.loads(text_data)
         if data_json['data_type'] == 'move':
             self.update_game_state(data_json)
+            print(self.game)
         elif data_json['data_type'] == 'enemy_id':
             self.create_new_game(data_json['enemy_id'])
 
@@ -75,17 +125,14 @@ class ChessConsumer(WebsocketConsumer):
             'color': event['updates']['color'],
             'new_position': event['updates']['new_position']
         }))
-        print(self.game)
         try:
-            print(self.new_game)
-            print(hex(id(self.new_game)))
+            print(self.game)
+            print(hex(id(self.game)))
         except:
             pass
 
     def disconnect(self, code):
         """ On ws disconnect deletes game assigned with the room_id """
-        if self.game:
-            self.game.delete()
         async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name,
             self.channel_name
