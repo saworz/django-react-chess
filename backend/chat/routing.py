e5b9f4f492a28212862cecc9f71951a6f17d7115
Michal Cz <michal.czechowicz7@gmail.com>
2024-02-01 23:43:55 +0100

    Fixed routing parameters error

--- backend/chat/routing.py
+++ backend/chat/routing.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('ws/game_chat/<other_user_id>', consumers.GameChatConsumer.as_asgi()),
-    path('ws/private_chat/<room_id>', consumers.PrivateChatConsumer.as_asgi())
+    path('ws/game_chat/<room_id>', consumers.GameChatConsumer.as_asgi()),
+    path('ws/private_chat/<other_user_id>', consumers.PrivateChatConsumer.as_asgi())
 ]
