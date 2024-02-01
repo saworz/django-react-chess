0d1b49d527519ec80a3dd6b3c3fc381fe882c5c0
Michal Cz <michal.czechowicz7@gmail.com>
2024-02-01 22:11:51 +0100

    Added in-game chat consumer

--- backend/chat/consumers.py
+++ backend/chat/consumers.py
@@ -6,7 +6,7 @@ import json
 
 
 class PrivateChatConsumer(WebsocketConsumer):
-
+    """Consumer responsible for private chat opened from friends menu"""
     def connect(self):
         self.other_user_id = self.scope['url_route']['kwargs']['other_user_id']
         self.logged_user_id = self.scope['user'].pk
@@ -53,3 +53,41 @@ class PrivateChatConsumer(WebsocketConsumer):
             self.save_chat_message(message)
 
 
+class GameChatConsumer(WebsocketConsumer):
+    """Consumer responsible for in-game chat"""
+    def connect(self):
+        room_id = self.scope['url_route']['kwargs']['room_id']
+        self.room_name = room_id
+        self.room_group_name = f"game_chat_{room_id}"
+
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        self.accept()
+
+    def receive(self, text_data):
+        data_json = json.loads(text_data)
+
+        if data_json['data_type'] == 'chat_message':
+            self.trigger_send_message(data_json['message'])
+
+    def trigger_send_message(self, message):
+        """ Triggers sending message via websocket """
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'send_message',
+                'message': message,
+                'sender': self.scope['user'].pk
+            }
+        )
+
+    def send_message(self, event):
+        """ Sends chat message """
+        self.send(text_data=json.dumps({
+            'type': 'chat_message',
+            'message': event['message'],
+            'sender': event['sender']
+        }))
